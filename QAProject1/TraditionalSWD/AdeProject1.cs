524b2c815a5a7f4828c3cd6d2c4cbeb5ac7161ba
bajfaj <bajfaj@yahoo.com>
2019-08-04 19:34:43 +0100

    For Ade

---
+++ QAProject1/TraditionalSWD/AdeProject1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace QAProject1.TraditionalSWD
+{
+    [TestClass]
+    public class AdeProject1
+    {
+        [TestMethod]
+        public void UserNavigation()
+        {
+            IWebDriver Driver;
+
+            Driver = new ChromeDriver();
+
+            Driver.Navigate().GoToUrl("https://qar3ad.eu.fleet-dashboard.com");
+            Driver.Manage().Window.Maximize();
+
+            Driver.FindElement(By.XPath("//*[@id='session_username']")).SendKeys("tester");
+
+            Driver.FindElement(By.Id("session_password")).SendKeys("YVP&-gEh8N");
+
+            Driver.FindElement(By.XPath("//*[@id='new_session']/fieldset/div[4]/div[1]/div/input")).Click();
+
+            Driver.Quit();
+        }
+    }
+}
